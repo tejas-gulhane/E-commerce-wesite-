2c46ea91514a2f5665b1f73b170673a4bae13663
tejas-gulhane <103639207+tejas-gulhane@users.noreply.github.com>
2022-08-15 11:22:35 +0530

    M5_task1

---
+++ App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+import { Fragment } from "react";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Store from "./components/Store";
+
+
+function App() {
+  return (
+      <Fragment>
+        <Header />
+        <Store />
+        <Footer />
+      </Fragment>
+  )
+}
+
+export default App;
